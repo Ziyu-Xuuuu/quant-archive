e6656af64920ddd00c916a495157bf69f0af36cc
Ziyu-Xuuuu <ziyuxu@umich.edu>
2025-03-02 23:48:10 -0800

    更新config里面的token，之前过了有效期

--- config/config.py
+++ config/config.py
@@ -1,5 +1,5 @@
 # config.py
-TUSHARE_TOKEN = "4d427aa0a10e23a2bf2bf2de0b240aa0005db0629feea9fa2a3bd6a8"
+TUSHARE_TOKEN = "16decc0759164f58b88f05b57d0627629bb1d3c75e30b5683b3ed2f3"
 
 # 如果要接入实盘券商，比如华宝、国泰君安等，这里可以存放相关配置
 BROKER_CONFIG = {
